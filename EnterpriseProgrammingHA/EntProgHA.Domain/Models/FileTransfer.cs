a568387b132ba445369f3ce73302562e148d8865
KURT ATTARD <kurt.attard.e23508@mcast.edu.mt>
2022-01-01 19:05:50 +0100

    Add FileTransfer Domain Model

---
+++ EnterpriseProgrammingHA/EntProgHA.Domain/Models/FileTransfer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntProgHA.Domain.Models
+{
+    public class FileTransfer
+    {
+        public int Id { get; set; }
+        public string SenderEmail { get; set; }
+        public string RecieverEmail { get; set; }
+        public string Title { get; set; }
+        public string Message { get; set; }
+        public string Password { get; set; }
+    }
+}
