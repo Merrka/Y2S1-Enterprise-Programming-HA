ac84093be94e68b7d4d97091e762e0dbdfa26811
KURT ATTARD <kurt.attard.e23508@mcast.edu.mt>
2022-01-04 00:01:56 +0100

    Completing some of the presentation layer.

---
+++ EnterpriseProgrammingHA/EntProgHA.Mvc/Controllers/FileTransferController.cs
@@ -0,0 +1,27 @@
+﻿using EntProgHA.Application.Interfaces;
+using EntProgHA.Application.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntProgHA.Mvc.Controllers
+{
+    public class FileTransferController : Controller
+    {
+        private IFileTransferService _fileTransferService;
+        public FileTransferController(IFileTransferService fileTransferService)
+        {
+            _fileTransferService = fileTransferService;
+        }
+
+        public IActionResult Index()
+        {
+            FileTransferViewModel model = _fileTransferService.GetFileTransfers();
+
+            return View();
+        }
+
+    }
+}
