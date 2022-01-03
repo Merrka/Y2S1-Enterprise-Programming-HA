f8869d1d9fbc82d90a3e9ff64e56d8f32337f41c
KURT ATTARD <kurt.attard.e23508@mcast.edu.mt>
2022-01-03 22:08:06 +0100

    Initial Commit of Infra.data project. update MVC for new FileTransferContext. added FileUrl to model

--- EnterpriseProgrammingHA/EntProgHA.Domain/Models/FileTransfer.cs
+++ EnterpriseProgrammingHA/EntProgHA.Domain/Models/FileTransfer.cs
@@ -12,5 +12,6 @@ namespace EntProgHA.Domain.Models
         public string Title { get; set; }
         public string Message { get; set; }
         public string Password { get; set; }
+        public string FileUrl { get; set; }
     }
 }
---
+++ EnterpriseProgrammingHA/EntProgHA.Infra.Data/Migrations/20220103210512_InitialMigration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EntProgHA.Infra.Data.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FileTransfers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SenderEmail = table.Column<string>(nullable: true),
+                    RecieverEmail = table.Column<string>(nullable: true),
+                    Title = table.Column<string>(nullable: true),
+                    Message = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true),
+                    FileUrl = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FileTransfers", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FileTransfers");
+        }
+    }
+}
